23fcc0549fdcc382ef26e1109fdd03e4132c6b99
AhmedEnaba1 <ahmedenaba@aucegypt.edu>
2023-11-18 17:09:44 +0200

    Fixed concurrency issues

--- Server_1/src/main.rs
+++ Server_1/src/main.rs
@@ -1,162 +1,3 @@
-// use async_std::net::UdpSocket;
-// use std::{net::SocketAddr, result};
-// use tokio::time::sleep;
-// use std::time::Duration;
-// use serde::{Serialize, Deserialize, de::Error};
-// use serde_json::Result;
-// use std::sync::Arc;
-
-// #[derive(Debug, Serialize, Deserialize)]
-// enum Message {
-//     Election(usize),
-//     Answer(usize),
-//     Coordinator(usize),
-//     Request(String),
-// }
-
-// async fn server_task(server1_id: usize) {
-    
-//     let server_address: SocketAddr = format!("127.0.0.1:8080")
-//         .parse()
-//         .expect("Failed to parse server address");
-//     let client_socket = UdpSocket::bind(&server_address)
-//         .await
-//         .expect("Failed to bind server socket");
-
-//     let mut buffer = [0; 1024];
-
-//     loop {
-//         if let Ok((bytes_received, client_address)) = client_socket.recv_from(&mut buffer).await {
-//             println!("Source: {}", client_address);
-//             let received_message: Message =
-//                 serde_json::from_slice(&buffer[..bytes_received]).expect("Failed to deserialize message");
-//             println!("Message: {:?}", received_message);
-
-//             match received_message {
-//                 Message::Request(request) => {
-//                     // Handle the actual request from the middleware
-//                     println!("Server {} received request: {}", server1_id, request);
-
-//                     // Start a new election
-//                     let leader = start_election(&client_socket, client_address, server1_id).await;
-//                     if leader{
-//                         let client_socket_clone = client_socket.clone();
-//                         let client_address_clone = client_address;
-//                         tokio::spawn(process_request(&client_socket_clone, client_address_clone));
-//                     }
-//                 }
-//                 _ => {}
-//             }
-
-//             // Clear the buffer for the next message
-//             buffer = [0; 1024];
-//         }
-//     }
-// }
-
-// async fn start_election(client_socket: &UdpSocket, client_address: SocketAddr, server1_id: usize)-> bool {
-//     println!("Server 1 is starting an election.");
-
-//     //The IDs of the other two servers is intialized to zeros at first
-//     let mut server2_id: usize = 0;
-//     let mut server3_id: usize = 0;
-
-//     //Socket to listen from Server2
-//     let socket2_address: SocketAddr = format!("127.0.0.1:2112")
-//         .parse()
-//         .expect("Failed to parse server address");
-//     let socket2 = UdpSocket::bind(&socket2_address)
-//         .await
-//         .expect("Failed to bind server socket");
-
-//     //Socket to listen from Server3
-//     let socket3_address: SocketAddr = format!("127.0.0.1:2113")
-//         .parse()
-//         .expect("Failed to parse server address");
-//     let socket3 = UdpSocket::bind(&socket3_address)
-//         .await
-//         .expect("Failed to bind server socket");
-
-//     // Broadcast Election messages to servers with higher IDs
-//     let election_message = Message::Election(server1_id);
-//     socket2
-//         .send_to(
-//             &serde_json::to_string(&election_message).unwrap().as_bytes(),
-//             "127.0.0.2:2112", // Replace with actual addresses of other servers
-//         )
-//         .await
-//         .expect("Failed to send Election message");
-
-//     socket3
-//         .send_to(
-//             &serde_json::to_string(&election_message).unwrap().as_bytes(),
-//             "127.0.0.3:2112", // Replace with actual addresses of other servers
-//         )
-//         .await
-//         .expect("Failed to send Election message");
-    
-//     // Set a timeout for receiving messages
-//     let timeout = Duration::from_millis(200);
-
-//     //Update the server IDs
-//     server2_id = receive_election_message(socket2, timeout).await.unwrap_or(0);
-//     server3_id = receive_election_message(socket3, timeout).await.unwrap_or(0);
-
-//     println!("IDs: {},{}", server2_id, server3_id);
-//     //Check if current server is Leader or not
-//     let leader = server1_id > server2_id && server1_id > server3_id;
-//     if leader {
-//         // Call a function to process the request (e.g., become the coordinator)
-//         println!("This Server is the Coordinator!");
-//     }
-    
-//     return leader;
-// }
-
-// async fn receive_election_message(socket: UdpSocket, timeout: Duration) -> Result<usize> {
-//     let mut buffer = [0; 1024];
-//     let mut server_id: usize = 0;
-
-//     // Set a timeout for receiving the message
-//     let result = tokio::time::timeout(timeout, async {
-//         if let Ok((bytes_received, _)) = socket.recv_from(&mut buffer).await {
-//             let received_message: Message =
-//                 serde_json::from_slice(&buffer[..bytes_received]).expect("Failed to deserialize message");
-
-//             if let Message::Election(id) = received_message {
-//                 server_id = id;
-//             }
-//         }
-//     })
-//     .await;
-
-//     result.map(|_| server_id).map_err(|_| serde_json::Error::custom("Timeout occurred"))
-// }
-
-// async fn process_request(client_socket: &UdpSocket, client_address: SocketAddr) {
-//     // Simulate processing the request
-//     sleep(Duration::from_millis(5000)).await;
-
-//     // Respond to the middleware (assuming the middleware's address is known)
-//     let response_message = Message::Request(format!("Response from Server 1"));
-//     client_socket
-//         .send_to(
-//             &serde_json::to_string(&response_message).unwrap().as_bytes(),
-//             client_address, // Replace with the actual client address
-//         )
-//         .await
-//         .expect("Failed to send response to client");
-// }
-
-// #[tokio::main]
-// async fn main() {
-//     let server1_id = 1; // Replace with the actual ID of the server
-
-//     let task = server_task(server1_id);
-//     let _ = tokio::join!(task);
-
-// }
-
 use async_std::net::UdpSocket;
 use std::{net::SocketAddr, sync::Arc};
 use tokio::time::sleep;
@@ -199,8 +40,9 @@ async fn server_task(server1_id: usize) {
                     // Start a new election
                     let client_socket_clone = Arc::clone(&client_socket);
                     let client_address_clone = client_address;
+                    let leader = start_election(server1_id).await;
                     tokio::spawn(async move {
-                        let leader = start_election(&client_socket_clone, client_address_clone, server1_id).await;
+                        //let leader = start_election(&client_socket_clone, client_address_clone, server1_id).await;
                         if leader {
                             process_request(&client_socket_clone, client_address_clone).await;
                         }
@@ -215,12 +57,8 @@ async fn server_task(server1_id: usize) {
     }
 }
 
-async fn start_election(client_socket: &Arc<UdpSocket>, client_address: SocketAddr, server1_id: usize) -> bool {
+async fn start_election(server1_id: usize) -> bool {
     println!("Server 1 is starting an election.");
-
-    // The IDs of the other two servers are initialized to zeros at first
-    let mut server2_id: usize = 0;
-    let mut server3_id: usize = 0;
 
     // Wrap the UdpSocket in Arc
     let socket2 = Arc::new(UdpSocket::bind("127.0.0.1:2112")
@@ -250,8 +88,8 @@ async fn start_election(client_socket: &Arc<UdpSocket>, client_address: SocketAd
     let timeout = Duration::from_millis(200);
 
     // Update the server IDs
-    server2_id = receive_election_message(Arc::clone(&socket2), timeout).await.unwrap_or(0);
-    server3_id = receive_election_message(Arc::clone(&socket3), timeout).await.unwrap_or(0);
+    let server2_id = receive_election_message(Arc::clone(&socket2), timeout).await.unwrap_or(0);
+    let server3_id = receive_election_message(Arc::clone(&socket3), timeout).await.unwrap_or(0);
 
     println!("IDs: {},{}", server2_id, server3_id);
 
--- Server_2/src/main.rs
+++ Server_2/src/main.rs
@@ -1,157 +1,3 @@
-// use async_std::net::UdpSocket;
-// use std::{net::SocketAddr, result};
-// use tokio::time::sleep;
-// use std::time::Duration;
-// use serde::{Serialize, Deserialize, de::Error};
-// use serde_json::Result;
-// use std::sync::Arc;
-
-// #[derive(Debug, Serialize, Deserialize)]
-// enum Message {
-//     Election(usize),
-//     Answer(usize),
-//     Coordinator(usize),
-//     Request(String),
-// }
-
-// async fn server_task(server2_id: usize) {
-    
-//     let server_address: SocketAddr = format!("127.0.0.2:8080")
-//         .parse()
-//         .expect("Failed to parse server address");
-//     let client_socket = UdpSocket::bind(&server_address)
-//         .await
-//         .expect("Failed to bind server socket");
-
-//     let mut buffer = [0; 1024];
-
-//     loop {
-//         if let Ok((bytes_received, client_address)) = client_socket.recv_from(&mut buffer).await {
-//             println!("Source: {}", client_address);
-//             let received_message: Message =
-//                 serde_json::from_slice(&buffer[..bytes_received]).expect("Failed to deserialize message");
-//             println!("Message: {:?}", received_message);
-
-//             match received_message {
-//                 Message::Request(request) => {
-//                     // Handle the actual request from the middleware
-//                     println!("Server {} received request: {}", server2_id, request);
-
-//                     // Start a new election
-//                     start_election(&client_socket, client_address, server2_id).await;
-//                 }
-//                 _ => {}
-//             }
-
-//             // Clear the buffer for the next message
-//             buffer = [0; 1024];
-//         }
-//     }
-// }
-
-// async fn start_election(client_socket: &UdpSocket, client_address: SocketAddr, server2_id: usize) {
-//     println!("Server 1 is starting an election.");
-
-//     //The IDs of the other two servers is intialized to zeros at first
-//     let mut server1_id: usize = 0;
-//     let mut server3_id: usize = 0;
-
-//     //Socket to listen from Server2
-//     let socket1_address: SocketAddr = format!("127.0.0.2:2112")
-//         .parse()
-//         .expect("Failed to parse server address");
-//     let socket1 = UdpSocket::bind(&socket1_address)
-//         .await
-//         .expect("Failed to bind server socket");
-
-//     //Socket to listen from Server3
-//     let socket3_address: SocketAddr = format!("127.0.0.2:2113")
-//         .parse()
-//         .expect("Failed to parse server address");
-//     let socket3 = UdpSocket::bind(&socket3_address)
-//         .await
-//         .expect("Failed to bind server socket");
-
-//     // Broadcast Election messages to servers with higher IDs
-//     let election_message = Message::Election(server2_id);
-//     socket1
-//         .send_to(
-//             &serde_json::to_string(&election_message).unwrap().as_bytes(),
-//             "127.0.0.1:2112", // Replace with actual addresses of other servers
-//         )
-//         .await
-//         .expect("Failed to send Election message");
-
-//     socket3
-//         .send_to(
-//             &serde_json::to_string(&election_message).unwrap().as_bytes(),
-//             "127.0.0.3:2112", // Replace with actual addresses of other servers
-//         )
-//         .await
-//         .expect("Failed to send Election message");
-    
-//     // Set a timeout for receiving messages
-//     let timeout = Duration::from_millis(200);
-
-//     //Update the server IDs
-//     server1_id = receive_election_message(socket1, timeout).await.unwrap_or(0);
-//     server3_id = receive_election_message(socket3, timeout).await.unwrap_or(0);
-
-//     println!("IDs: {},{}", server1_id, server3_id);
-//     //Check if current server is Leader or not
-//     if server2_id > server1_id && server2_id > server3_id {
-//         // Call a function to process the request (e.g., become the coordinator)
-//         println!("This Server is the Coordinator!");
-//         process_request(&client_socket, client_address).await;
-//     }
-
-// }
-
-// async fn receive_election_message(socket: UdpSocket, timeout: Duration) -> Result<usize> {
-//     let mut buffer = [0; 1024];
-//     let mut server_id: usize = 0;
-
-//     // Set a timeout for receiving the message
-//     let result = tokio::time::timeout(timeout, async {
-//         if let Ok((bytes_received, _)) = socket.recv_from(&mut buffer).await {
-//             let received_message: Message =
-//                 serde_json::from_slice(&buffer[..bytes_received]).expect("Failed to deserialize message");
-
-//             if let Message::Election(id) = received_message {
-//                 server_id = id;
-//             }
-//         }
-//     })
-//     .await;
-
-//     result.map(|_| server_id).map_err(|_| serde_json::Error::custom("Timeout occurred"))
-// }
-
-// async fn process_request(client_socket: &UdpSocket, client_address: SocketAddr) {
-//     // Simulate processing the request
-//     sleep(Duration::from_millis(5000)).await;
-
-//     // Respond to the middleware (assuming the middleware's address is known)
-//     let response_message = Message::Request(format!("Response from Server 2"));
-//     client_socket
-//         .send_to(
-//             &serde_json::to_string(&response_message).unwrap().as_bytes(),
-//             client_address, // Replace with the actual client address
-//         )
-//         .await
-//         .expect("Failed to send response to client");
-// }
-
-// #[tokio::main]
-// async fn main() {
-//     let server1_id = 2; // Replace with the actual ID of the server
-
-//     let task = server_task(server1_id);
-//     let _ = tokio::join!(task);
-
-// }
-
-
 use async_std::net::UdpSocket;
 use std::{net::SocketAddr, sync::Arc};
 use tokio::time::sleep;
@@ -194,8 +40,9 @@ async fn server_task(server2_id: usize) {
                     // Start a new election
                     let client_socket_clone = Arc::clone(&client_socket);
                     let client_address_clone = client_address;
+                    let leader = start_election(server2_id).await;
                     tokio::spawn(async move {
-                        let leader = start_election(&client_socket_clone, client_address_clone, server2_id).await;
+                        //let leader = start_election(&client_socket_clone, client_address_clone, server1_id).await;
                         if leader {
                             process_request(&client_socket_clone, client_address_clone).await;
                         }
@@ -210,12 +57,8 @@ async fn server_task(server2_id: usize) {
     }
 }
 
-async fn start_election(client_socket: &Arc<UdpSocket>, client_address: SocketAddr, server2_id: usize) -> bool {
+async fn start_election(server2_id: usize) -> bool {
     println!("Server 2 is starting an election.");
-
-    // The IDs of the other two servers are initialized to zeros at first
-    let mut server1_id: usize = 0;
-    let mut server3_id: usize = 0;
 
     // Wrap the UdpSocket in Arc
     let socket1 = Arc::new(UdpSocket::bind("127.0.0.2:2112")
@@ -245,8 +88,8 @@ async fn start_election(client_socket: &Arc<UdpSocket>, client_address: SocketAd
     let timeout = Duration::from_millis(200);
 
     // Update the server IDs
-    server1_id = receive_election_message(Arc::clone(&socket1), timeout).await.unwrap_or(0);
-    server3_id = receive_election_message(Arc::clone(&socket3), timeout).await.unwrap_or(0);
+    let server1_id = receive_election_message(Arc::clone(&socket1), timeout).await.unwrap_or(0);
+    let server3_id = receive_election_message(Arc::clone(&socket3), timeout).await.unwrap_or(0);
 
     println!("IDs: {},{}", server1_id, server3_id);
 
